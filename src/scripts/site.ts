1d90c6726c278f0fead1768cd3717e1f53f22556
DLYZZT <91056284+DLYZZT@users.noreply.github.com>
2026-08-11 01:20:45 -0700

    docs: update website for v0.1.7

--- src/scripts/site.ts
+++ src/scripts/site.ts
@@ -1,8 +1,8 @@
 import { withBase } from "../utils/paths";
 
 const REPO = "DLYZZT/pi-desktop";
-const FALLBACK_VERSION = "0.1.6";
-const RELEASE_CACHE_KEY = "pi-site-release-v0.1.6";
+const FALLBACK_VERSION = "0.1.7";
+const RELEASE_CACHE_KEY = "pi-site-release-v0.1.7";
 
 type Platform = {
   os: "mac" | "windows" | "linux" | "unknown";
